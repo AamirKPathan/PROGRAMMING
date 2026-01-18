07a0ab5a5907432be24e9e96d7cad448f77e3dde
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-18 02:23:16 +0000

    Jan 17, 2026, 9:23 PM

--- WebDevelopment/JS/Basics/35.functionExpressions/index.js
+++ WebDevelopment/JS/Basics/35.functionExpressions/index.js
@@ -1,3 +1,4 @@
 // function declaration = define a resuable block of code that performs a specific task
 
-// function expression = a way to def
+// function expression = a way to define a function
+
