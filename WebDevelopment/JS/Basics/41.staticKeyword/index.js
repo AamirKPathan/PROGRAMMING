464f21a4db8953a8e731f57e860d7c3800b169aa
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-18 18:55:12 +0000

    Jan 18, 2026, 1:55 PM

--- WebDevelopment/JS/Basics/41.staticKeyword/index.js
+++ WebDevelopment/JS/Basics/41.staticKeyword/index.js
@@ -1,24 +1,14 @@
-// classes in JavaScript
-// A class is a blueprint for creating objects with predefined properties and methods.
-class Person {
-    constructor(firstName, lastName, age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
+// static = A keyword used to define static methods or properties in a class.
+// Static methods and properties belong to the class itself, not to any specific instance of the class.
+
+class MathUtils {
+    static add(a, b) {
+        return a + b;
     }
-    fullName() {
-        return `${this.firstName} ${this.lastName}`;
+
+    static subtract(a, b) {
+        return a - b;
     }
+
+    static pi = 3.14159;
 }
-const person1 = new Person('John', 'Doe', 30);
-console.log(person1.firstName); // John
-console.log(person1.fullName()); // John Doe
-const person2 = new Person('Jane', 'Smith', 25);
-console.log(person2.firstName); // Jane
-console.log(person2.fullName()); // Jane Smith
-const person3 = new Person('Alice', 'Johnson', 28);
-console.log(person3.firstName); // Alice
-console.log(person3.fullName()); // Alice Johnson
-const person4 = new Person('Bob', 'Brown', 35);
-console.log(person4.firstName); // Bob
-console.log(person4.fullName()); // Bob Brown
