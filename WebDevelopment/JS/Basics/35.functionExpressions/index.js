d2bbb83dfcec376af154aa058da141590db281b5
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-18 02:23:54 +0000

    Jan 17, 2026, 9:23 PM

--- WebDevelopment/JS/Basics/35.functionExpressions/index.js
+++ WebDevelopment/JS/Basics/35.functionExpressions/index.js
@@ -2,3 +2,27 @@
 
 // function expression = a way to define a function as values or variables
 
+const add = function (a, b) {
+    return a + b;
+};
+const sum = add(2, 3);
+console.log(sum); // 5
+// function expression can be anonymous (without a name) or named
+const multiply = function multiplyNumbers(a, b) {
+    return a * b;
+};
+const product = multiply(4, 5);
+console.log(product); // 20
+// function expressions can be used as arguments to other functions (callbacks)
+function calculate(a, b, operation) {
+    return operation(a, b);
+}
+const difference = calculate(10, 4, function (a, b) {
+
+    return a - b;
+});
+console.log(difference); // 6
+const quotient = calculate(20, 5, function divideNumbers(a, b) {
+    return a / b;
+});
+console.log(quotient); // 4
