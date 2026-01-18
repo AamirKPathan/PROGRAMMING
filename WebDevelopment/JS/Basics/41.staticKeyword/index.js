fd0780b663a53dd6378de48a27d40974550714fc
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-18 18:55:24 +0000

    Jan 18, 2026, 1:55 PM

--- WebDevelopment/JS/Basics/41.staticKeyword/index.js
+++ WebDevelopment/JS/Basics/41.staticKeyword/index.js
@@ -12,3 +12,10 @@ class MathUtils {
 
     static pi = 3.14159;
 }
+console.log(MathUtils.add(5, 3)); // 8
+console.log(MathUtils.subtract(10, 4)); // 6
+console.log(MathUtils.pi); // 3.14159
+const mathInstance = new MathUtils();
+// console.log(mathInstance.add(5, 3)); // Error: mathInstance.add is not a function
+// console.log(mathInstance.pi); // undefined
+// Static methods and properties can be accessed directly through the class without creating an instance.
