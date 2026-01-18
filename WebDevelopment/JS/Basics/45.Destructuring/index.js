f7d5643a181049e29c73e9e00fd06a1e41e07c78
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-18 20:15:48 +0000

    Jan 18, 2026, 3:15 PM

--- WebDevelopment/JS/Basics/45.Destructuring/index.js
+++ WebDevelopment/JS/Basics/45.Destructuring/index.js
@@ -1,3 +1,12 @@
 // destructuring = extract values from arrays and objects, then assign them to variables in a convenient way
 // [] perform array destructuring
 // {} perform object destructuring
+
+// array destructuring
+const colors = ['red', 'green', 'blue'];
+const [firstColor, secondColor, thirdColor] = colors;
+console.log(firstColor); // red
+console.log(secondColor); // green
+console.log(thirdColor); // blue
+const [primary, , tertiary] = colors; // skip the second element
+console.log(primary); // red
