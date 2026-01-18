803dd4a9468d80cf7493ab0014699ca30172fe5b
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-18 19:48:47 +0000

    Jan 18, 2026, 2:48 PM

--- WebDevelopment/JS/Basics/42.inheritance/index.js
+++ WebDevelopment/JS/Basics/42.inheritance/index.js
@@ -1,1 +1,21 @@
-//
+// inheritance allows a new class to inherit properties and methods from an existing class
+
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+    speak() {
+        return `${this.name} makes a noise.`;
+    }
+}
+class Dog extends Animal {
+    speak() {
+        return `${this.name} barks.`;
+    }
+}
+const animal1 = new Animal('Generic Animal');
+console.log(animal1.speak()); // Generic Animal makes a noise.
+const dog1 = new Dog('Rex');
+console.log(dog1.speak()); // Rex barks.
+// The Dog class inherits from the Animal class and overrides the speak method.
+class Cat extends
