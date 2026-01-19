03365fd6a80e3329a80789ddd4489d7fbf5f6679
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-19 23:25:55 +0000

    Jan 19, 2026, 6:25 PM

--- WebDevelopment/JS/Basics/50.dateObjects/index.js
+++ WebDevelopment/JS/Basics/50.dateObjects/index.js
@@ -1,1 +1,9 @@
-// date objects are objects that r
+// date objects are objects that represent dates and times
+
+// create a new date object for the current date and time
+const now = new Date();
+console.log("Current date and time:", now);
+// create a date object for a specific date
+const specificDate = new Date('2022-12-25T10:30:00');
+console.log("Specific date and time:", specificDate);
+// get individual components of the date
