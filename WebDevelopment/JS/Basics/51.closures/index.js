8a9127115398aa173620c98d72dd3e7dea0c174f
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-20 00:11:52 +0000

    Jan 19, 2026, 7:11 PM

--- WebDevelopment/JS/Basics/51.closures/index.js
+++ WebDevelopment/JS/Basics/51.closures/index.js
@@ -50,3 +50,13 @@ console.log(person.age); // undefined
 //the name and age variables are private and cannot be accessed directly
 //only through the provided methods
 //closures can also be used to create function factories
+function makeAdder(x) {
+    return function(y) {
+        return x + y;
+    }
+}
+const add5 = makeAdder(5);
+const add10 = makeAdder(10);
+console.log(add5(2)); // 7
+console.log(add10(2)); // 12
+
