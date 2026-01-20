0d9be0147d97276ae9ed3f82c73779b2d72495d9
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-20 00:11:15 +0000

    Jan 19, 2026, 7:11 PM

--- WebDevelopment/JS/Basics/51.closures/index.js
+++ WebDevelopment/JS/Basics/51.closures/index.js
@@ -1,1 +1,33 @@
-/
+//closures are functions defined inside other functions
+function outerFunction(outerVariable) {
+    return function innerFunction(innerVariable) {
+        console.log('Outer Variable: ' + outerVariable);
+        console.log('Inner Variable: ' + innerVariable);
+    }
+}
+const newFunction = outerFunction('outside');
+newFunction('inside');
+//or you can call it like this
+outerFunction('outside')('inside');
+//closures can remember the environment in which they were created
+function makeCounter() {
+    let count = 0;
+    return function() {
+        count++;
+        return count;
+    }
+}
+const counter = makeCounter();
+
+console.log(counter()); // 1
+
+console.log(counter()); // 2
+console.log(counter()); // 3
+//each counter has its own lexical environment
+const counter1 = makeCounter();
+console.log(counter1()); // 1
+console.log(counter1()); // 2
+const counter2 = makeCounter();
+console.log(counter2()); // 1
+console.log(counter1()); // 3
+console.log(counter2()); // 2
