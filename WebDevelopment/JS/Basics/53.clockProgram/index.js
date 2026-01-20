09a9fb334143010579039921ab8b79a90c1c61c4
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-20 01:15:24 +0000

    Jan 19, 2026, 8:15 PM

--- WebDevelopment/JS/Basics/53.clockProgram/index.js
+++ WebDevelopment/JS/Basics/53.clockProgram/index.js
@@ -1,3 +1,18 @@
 // Clock program
 function updateClock() {
-    
+    const now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    document.getElementById('hours').textContent = hours;
+    document.getElementById('minutes').textContent = minutes;
+    document.getElementById('seconds').textContent = seconds;
+    document.getElementById('ampm').textContent = ampm;
+    setTimeout(updateClock, 1000);
+}
+updateClock();
