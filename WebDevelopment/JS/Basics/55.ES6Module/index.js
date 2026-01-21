921969d591d799ee4e3e7fc5803f731ec9122f04
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-21 19:21:00 +0000

    Jan 21, 2026, 2:21 PM

---
+++ WebDevelopment/JS/Basics/55.ES6Module/index.js
@@ -0,0 +1,9 @@
+//ES6 Module = A file that contains code that can be exported to and imported from other files
+
+//Exporting a function
+export function add(a, b) {
+    return a + b;
+}
+
+//Exporting a constant
+export const PI = 3.14159; 
