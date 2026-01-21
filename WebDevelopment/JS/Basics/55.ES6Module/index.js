33ad0d40cd597a55d2b6a20484b389a026494b49
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-21 19:21:22 +0000

    Jan 21, 2026, 2:21 PM

--- WebDevelopment/JS/Basics/55.ES6Module/index.js
+++ WebDevelopment/JS/Basics/55.ES6Module/index.js
@@ -6,4 +6,21 @@ export function add(a, b) {
 }
 
 //Exporting a constant
-export const PI = 3.14159; 
+export const PI = 3.14159;
+//Exporting a class
+export class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    area() {
+        return PI * this.radius * this.radius;
+    }
+}
+
+//Default export
+export default function subtract(a, b) {
+    return a - b;
+}
+//--- IGNORE ---
+//End of file
