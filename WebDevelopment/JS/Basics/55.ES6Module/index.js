68238ce574dd80910bef000347e915563552f341
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-21 19:21:48 +0000

    Jan 21, 2026, 2:21 PM

--- WebDevelopment/JS/Basics/55.ES6Module/index.js
+++ WebDevelopment/JS/Basics/55.ES6Module/index.js
@@ -1,26 +1,3 @@
 //ES6 Module = A file that contains code that can be exported to and imported from other files
 
-//Exporting a function
-export function add(a, b) {
-    return a + b;
-}
-
-//Exporting a constant
-export const PI = 3.14159;
-//Exporting a class
-export class Circle {
-    constructor(radius) {
-        this.radius = radius;
-    }
-
-    area() {
-        return PI * this.radius * this.radius;
-    }
-}
-
-//Default export
-export default function subtract(a, b) {
-    return a - b;
-}
-//--- IGNORE ---
-//End of file
+import {} from './mathUtil.js';
