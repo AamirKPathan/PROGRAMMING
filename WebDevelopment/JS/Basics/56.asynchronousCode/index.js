022ec821cd8b9f5e552ac6af2d725674d9acc189
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-22 02:32:08 +0000

    Jan 21, 2026, 9:32 PM

--- WebDevelopment/JS/Basics/56.asynchronousCode/index.js
+++ WebDevelopment/JS/Basics/56.asynchronousCode/index.js
@@ -1,1 +1,3 @@
-// synchronous is code that executes 
+// synchronous is code that executes consecutively in a sequential manner
+
+// asynchronous is code that executes independently of the main program flow, allowing other operations to continue while waiting for the asynchronous operation to complete
