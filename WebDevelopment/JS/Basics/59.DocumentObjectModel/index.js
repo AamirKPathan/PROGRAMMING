638c9d06382dcbe973852816d92e816deaaf1cea
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-23 00:06:07 +0000

    Jan 22, 2026, 7:06 PM

--- WebDevelopment/JS/Basics/59.DocumentObjectModel/index.js
+++ WebDevelopment/JS/Basics/59.DocumentObjectModel/index.js
@@ -1,1 +1,6 @@
-// DOM
+// DOM = Document Object Model
+// The DOM is a programming interface for web documents.
+// It represents the page so that programs can change the document structure, style, and content.
+// The DOM represents the document as nodes and objects.
+// This allows programming languages to interact with the page content dynamically.
+
