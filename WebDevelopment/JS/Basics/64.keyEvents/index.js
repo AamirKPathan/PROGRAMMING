e48a9c2add8f70cbe849a0c2e91972d04a7dcf64
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-23 01:21:55 +0000

    Jan 22, 2026, 8:21 PM

--- WebDevelopment/JS/Basics/64.keyEvents/index.js
+++ WebDevelopment/JS/Basics/64.keyEvents/index.js
@@ -1,4 +1,3 @@
 // eventListener
 // listen for specific events like key presses, mouse clicks, etc.
 // common key events: keydown, keyup, keypress
-const myInput = document.getElementById('myInput');
