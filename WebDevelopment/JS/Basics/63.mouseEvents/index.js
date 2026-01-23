2b061f08ee887af62a81c1587814f965673bee6b
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-23 01:21:04 +0000

    Jan 22, 2026, 8:21 PM

--- WebDevelopment/JS/Basics/63.mouseEvents/index.js
+++ WebDevelopment/JS/Basics/63.mouseEvents/index.js
@@ -1,18 +1,5 @@
-// eventListener = listen for specific evenetts like mouse clicks, mouse moves, key presses, etc.
+// event listener
+// listen for specific events like mouse clicks, mouse moves, key presses, etc.
 // common mouse events: click, dblclick, mouseover, mouseout, mousedown, mouseup, mousemove
-
 const myBox = document.getElementById('myBox');
 const myButton = document.getElementById('myButton');
-
-myBox.addEventListener('click', event => {
-    event.target.style.backgroundColor = 'tomato';
-    event.target.textContent = 'Ouch🤕';
-});
-myBox.addEventListener('mouseover', event => {
-    event.target.style.backgroundColor = 'yellow';
-    event.target.textContent = 'Don\'t do it!😱';
-});
-myBox.addEventListener('mouseout', event => {
-    event.target.style.backgroundColor = 'lightblue';
-    event.target.textContent = 'Click Me😎';
-});
