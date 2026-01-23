b13ffdde61ca1ea4fdebe1fa82d9f1bb97b60dc3
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-23 00:52:32 +0000

    Jan 22, 2026, 7:52 PM

--- WebDevelopment/JS/Basics/62.addAndChangeHTML/index.js
+++ WebDevelopment/JS/Basics/62.addAndChangeHTML/index.js
@@ -3,4 +3,6 @@ const newDiv = document.createElement('div'); // Create a new div element
 newDiv.innerHTML = '<h2>This is a new heading</h2><p>This paragraph was added using JavaScript.</p>'; // Add HTML content
 document.body.appendChild(newDiv); // Append the new div to the body
 // STEP 2 Add Attributes
+newDiv.setAttribute('class', 'new-content'); // Add a class attribute
+newDiv.setAttribute('id', 'content-1'); // Add an id attribute
 // STEP 3 APPEND ELEMENT TO DOM
