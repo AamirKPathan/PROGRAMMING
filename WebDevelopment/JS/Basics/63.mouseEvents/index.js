b84c346096070508ae50f1b27df595e648f90aba
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-23 00:56:34 +0000

    Jan 22, 2026, 7:56 PM

--- WebDevelopment/JS/Basics/63.mouseEvents/index.js
+++ WebDevelopment/JS/Basics/63.mouseEvents/index.js
@@ -1,1 +1,7 @@
 // eventListener = listen for specific evenetts like mouse clicks, mouse moves, key presses, etc.
+// common mouse events: click, dblclick, mouseover, mouseout, mousedown, mouseup, mousemove
+
+const myBox = document.getElementById('myBox');
+const myButton = document.getElementById('myButton');
+
+myb
