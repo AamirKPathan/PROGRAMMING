d951c0a1d0a99c93418aa76873152f3da0b92712
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-23 00:15:09 +0000

    Jan 22, 2026, 7:15 PM

--- WebDevelopment/JS/Basics/61.DOMNavigation/index.js
+++ WebDevelopment/JS/Basics/61.DOMNavigation/index.js
@@ -1,1 +1,8 @@
-// DOM Navigation = The process of moving through the DOM tree structure    
+// DOM Navigation = The process of moving through the DOM tree structure
+// to access and manipulate elements, attributes, and text nodes.
+// It allows developers to traverse parent, child, and sibling relationships
+// between nodes in the DOM.
+// Common DOM Navigation Properties and Methods:
+// 1. parentNode: Accesses the parent node of a specified element.
+// 2. childNodes: Returns a collection of child nodes of a specified element.
+
