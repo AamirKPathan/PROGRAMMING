d3ab703e4228c45102993d2cf2ab9e2d21f56269
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-24 01:08:19 +0000

    Jan 23, 2026, 8:08 PM

--- WebDevelopment/JS/Basics/67.classLists/index.js
+++ WebDevelopment/JS/Basics/67.classLists/index.js
@@ -9,3 +9,13 @@
 //replace()
 
 const element = document.querySelector('.my-element');
+console.log('Initial class list:', element.classList);
+// Adding a class
+element.classList.add('new-class');
+console.log('After adding new-class:', element.classList);
+// Removing a class
+element.classList.remove('old-class');
+console.log('After removing old-class:', element.classList);
+// Toggling a class
+element.classList.toggle('toggle-class');
+console.log('After toggling toggle-class:', element.classList);
