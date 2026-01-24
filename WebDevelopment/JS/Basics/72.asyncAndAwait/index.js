89560140c01693ef4ef69066115a067e00e309bc
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-24 22:41:44 +0000

    Jan 24, 2026, 5:41 PM

--- WebDevelopment/JS/Basics/72.asyncAndAwait/index.js
+++ WebDevelopment/JS/Basics/72.asyncAndAwait/index.js
@@ -1,1 +1,18 @@
-// Asyn
+// Async And Await
+
+// Async and Await are syntactic sugar built on top of Promises in JavaScript. They provide a more readable and straightforward way to work with asynchronous code.
+// An async function always returns a Promise. The await keyword can only be used inside an async function and it pauses the execution of the function until the Promise is resolved or rejected.
+// Example of using async and await:
+
+function fetchData() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Data fetched");
+        }, 2000);
+    });
+}
+async function getData() {
+    console.log("Fetching data...");
+    const data = await fetchData();
+    console.log(data);
+}   
