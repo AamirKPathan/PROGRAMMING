aef9c2f6ee050b3c4c38430a3a6ed88b1f64a851
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-24 01:41:45 +0000

    Jan 23, 2026, 8:41 PM

---
+++ WebDevelopment/JS/Basics/71.promises/index.js
@@ -0,0 +1,49 @@
+// Callback hell is a situation in javascript where multiple nested callbacks make the code hard to read and maintain.
+// This often happens when dealing with asynchronous operations like API calls, timers, or event handling.
+// Example of callback hell:
+setTimeout(() => {
+    console.log("First callback");
+    setTimeout(() => {
+        console.log("Second callback");
+        setTimeout(() => {
+            console.log("Third callback");
+        }, 1000);
+    }, 1000);
+}, 1000);
+
+// To avoid callback hell, we can use Promises or async/await syntax.
+// Example using Promises:
+function firstCallback() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("First callback");
+            resolve();
+        }, 1000);
+    });
+}
+function secondCallback() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Second callback");
+            resolve();
+        }, 1000);
+    });
+}
+function thirdCallback() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Third callback");
+            resolve();
+        }, 1000);
+    });
+}
+firstCallback()
+    .then(() => secondCallback())
+    .then(() => thirdCallback());
+// Example using async/await:
+async function executeCallbacks() {
+    await firstCallback();
+    await secondCallback();
+    await thirdCallback();
+}
+executeCallbacks();
