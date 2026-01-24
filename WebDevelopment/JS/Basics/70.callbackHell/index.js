87be038b4686ec11b75b89055b72df94e13ccce9
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-24 01:41:14 +0000

    Jan 23, 2026, 8:41 PM

--- WebDevelopment/JS/Basics/70.callbackHell/index.js
+++ WebDevelopment/JS/Basics/70.callbackHell/index.js
@@ -10,3 +10,30 @@ setTimeout(() => {
         }, 1000);
     }, 1000);
 }, 1000);
+
+// To avoid callback hell, we can use Promises or async/await syntax.
+// Example using Promises:
+function firstCallback() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("First callback");
+            resolve();
+        }, 1000);
+    });
+}
+function secondCallback() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Second callback");
+            resolve();
+        }, 1000);
+    });
+}
+function thirdCallback() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Third callback");
+            resolve();
+        }, 1000);
+    });
+}
