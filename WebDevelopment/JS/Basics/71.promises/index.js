ae91b9bb6995e6ee10a38c7b88c63e1cab70851f
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-24 02:03:00 +0000

    Jan 23, 2026, 9:03 PM

--- WebDevelopment/JS/Basics/71.promises/index.js
+++ WebDevelopment/JS/Basics/71.promises/index.js
@@ -27,4 +27,43 @@ myPromise
 // DO THESE CHORES IN ORDER
 // 1. Walk The Dog
 // 2 . Wash The Car
-/
+// 3. Mow The Lawn
+function walkTheDog() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Walked the dog.");
+            resolve();
+        }, 1000);
+    });
+}
+
+function washTheCar() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Washed the car.");
+            resolve();
+        }, 1500);
+    });
+}
+function mowTheLawn() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Mowed the lawn.");
+            resolve();
+        }, 1200);
+    });
+}
+// Chaining Promises
+walkTheDog()
+    .then(() => washTheCar())
+    .then(() => mowTheLawn())
+    .then(() => {
+        console.log("All chores are done!");
+    });
+// Using async/await
+async function doChores() {
+    await walkTheDog();
+    await washTheCar();
+    await mowTheLawn();
+    console.log("All chores are done!");
+}
