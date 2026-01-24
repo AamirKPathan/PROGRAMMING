b6b38c4832f561997b8146dd3ea18a228a6c692d
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-24 01:07:48 +0000

    Jan 23, 2026, 8:07 PM

---
+++ WebDevelopment/JS/Basics/67.classLists/index.js
@@ -0,0 +1,6 @@
+// classList = Element property in JavaScript
+// It returns the class names of an element as a DOMTokenList object
+// It has methods to add, remove, toggle, and check for classes on an element
+
+// Select the element
+const myElement = document
