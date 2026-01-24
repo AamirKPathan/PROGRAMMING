8f1265bb49e1cc06f96d4c56a00cdf23e6ee6039
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-24 00:15:33 +0000

    Jan 23, 2026, 7:15 PM

--- WebDevelopment/JS/Basics/66. Generics/index.js
+++ WebDevelopment/JS/Basics/66. Generics/index.js
@@ -1,10 +1,1 @@
-//hiding and showing HTML elements using JavaScript
-const toggleButton = document.getElementById('toggleButton');
-const content = document.getElementById('content');
-toggleButton.addEventListener('click', () => {
-    if (content.style.display === 'none') {
-        content.style.display = 'block'; // Show the content
-    } else {
-        content.style.display = 'none'; // Hide the content
-    }
-});
+// generics is a concept where u can write a cl
