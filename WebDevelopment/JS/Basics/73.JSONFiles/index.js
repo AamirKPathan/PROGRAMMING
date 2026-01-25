3d47f9f234758fa5a777422bf7da16b9b121a5ae
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-25 16:41:35 +0000

    Jan 25, 2026, 11:41 AM

--- WebDevelopment/JS/Basics/73.JSONFiles/index.js
+++ WebDevelopment/JS/Basics/73.JSONFiles/index.js
@@ -2,4 +2,16 @@
 //It is a lightweight data interchange format that is easy for humans to read and write
 //and easy for machines to parse and generate.
 //JSON is often used to transmit data between a server and a web application as text.
-//Example of JSON data:
+//Example of JSON data
+fetch("names.json")
+    .then(response => response.json())
+    .then(data => {
+        const list = document.getElementById("nameList");
+
+        data.names.forEach(person => {
+            const li = document.createElement("li");
+            li.textContent = person;
+            list.appendChild(li);
+        });
+    })
+    .catch(error => console.error("Error loading JSON:", error));
