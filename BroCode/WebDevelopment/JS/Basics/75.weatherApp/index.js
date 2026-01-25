15d211a419e379d6a728dc4d86dc6678abf617af
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-25 17:39:29 +0000

    Jan 25, 2026, 12:39 PM

--- BroCode/WebDevelopment/JS/Basics/75.weatherApp/index.js
+++ BroCode/WebDevelopment/JS/Basics/75.weatherApp/index.js
@@ -3,3 +3,5 @@ const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput")
 const card = document.querySelector(".card")
 const apiKey = "36b9c3d5978540f659c374378104b089";
+
+weatherForm.addEventListener();
