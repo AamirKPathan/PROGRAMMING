929d5d585d3a26f9a50c998a17b790e17fed5d76
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-25 18:02:30 +0000

    Jan 25, 2026, 1:02 PM

--- BroCode/WebDevelopment/JS/Basics/75.weatherApp/index.js
+++ BroCode/WebDevelopment/JS/Basics/75.weatherApp/index.js
@@ -56,7 +56,7 @@ function displayWeatherInfo(data){
 
     cityDisplay.textContent = city;
     tempDisplay.textContent = `${(temp-273.15).toFixed(1)}°C`
-    humidityDisplay.textContent = `hum`
+    humidityDisplay.textContent = `Humidity:`
 
     cityDisplay.classList.add("cityDisplay");
     tempDisplay.classList.add("tempDisplay");
