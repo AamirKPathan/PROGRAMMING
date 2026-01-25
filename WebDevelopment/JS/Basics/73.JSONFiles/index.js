5a6387398426c98b5223d647e87454090e3d8e07
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-25 16:39:56 +0000

    Jan 25, 2026, 11:39 AM

--- WebDevelopment/JS/Basics/73.JSONFiles/index.js
+++ WebDevelopment/JS/Basics/73.JSONFiles/index.js
@@ -3,14 +3,3 @@
 //and easy for machines to parse and generate.
 //JSON is often used to transmit data between a server and a web application as text.
 //Example of JSON data:
-const jsonData = `{
-    "name": "John Doe",
-    "age": 30,
-    "isStudent": false,
-    "courses": ["Math", "Science", "History"],
-    "address": {
-        "street": "123 Main St",
-        "city": "Anytown",
-        "zip": "12345"
-    }
-}`;
