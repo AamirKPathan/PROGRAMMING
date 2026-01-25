c9626c96ac8884229b2332f046c73212d8d4a90c
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-25 16:39:23 +0000

    Jan 25, 2026, 11:39 AM

--- WebDevelopment/JS/Basics/73.JSONFiles/index.js
+++ WebDevelopment/JS/Basics/73.JSONFiles/index.js
@@ -1,1 +1,16 @@
-//JSO
+//JSON = javascript object notation
+//It is a lightweight data interchange format that is easy for humans to read and write
+//and easy for machines to parse and generate.
+//JSON is often used to transmit data between a server and a web application as text.
+//Example of JSON data:
+const jsonData = `{
+    "name": "John Doe",
+    "age": 30,
+    "isStudent": false,
+    "courses": ["Math", "Science", "History"],
+    "address": {
+        "street": "123 Main St",
+        "city": "Anytown",
+        "zip": "12345"
+    }
+}`;
