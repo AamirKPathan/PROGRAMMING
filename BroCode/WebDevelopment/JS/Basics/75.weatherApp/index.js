f87ea16986dcfdc9bb4f3a18342098fc572438c2
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-25 17:46:55 +0000

    Jan 25, 2026, 12:46 PM

--- BroCode/WebDevelopment/JS/Basics/75.weatherApp/index.js
+++ BroCode/WebDevelopment/JS/Basics/75.weatherApp/index.js
@@ -28,7 +28,7 @@ weatherForm.addEventListener("submit", async event => {
 
 async function getWeatherData(city){
 
-    const apiUrl = `https://api.`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weat`;
 }
 
 function displayWeatherInfo(data){
