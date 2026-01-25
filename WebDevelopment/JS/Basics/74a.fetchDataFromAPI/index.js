638bd07458fee05b3f68bb16d29a9ee7e93637dd
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-25 16:43:11 +0000

    Jan 25, 2026, 11:43 AM

--- WebDevelopment/JS/Basics/74a.fetchDataFromAPI/index.js
+++ WebDevelopment/JS/Basics/74a.fetchDataFromAPI/index.js
@@ -16,3 +16,28 @@ async function fetchDataFromAPI() {
 }
 fetchDataFromAPI();
 // In this example, we define an async function fetchDataFromAPI that uses fetch to get a post from a placeholder API. We check if the response is okay, then parse the JSON data and log it to the console. If there's an error during the fetch operation, it is caught and logged.
+// You can also use fetch to send data to an API using POST method:
+async function postDataToAPI() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: 'foo',
+                body: 'bar',
+                userId: 1
+            })
+        });
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
+        console.log('Data posted to API:', data);
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+postDataToAPI();
+// In this example, we send a POST request to the same placeholder API with some JSON data. We set the method to 'POST', specify the content type in the headers, and include the data in the body of the request. The response is then parsed and logged similarly to the GET request.
