2ba8f55ded86005788b26c75a79e6dd9b2cebc35
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-01-25 18:08:21 +0000

    Jan 25, 2026, 1:08 PM

--- BroCode/WebDevelopment/JS/Basics/75.weatherApp/index.js
+++ BroCode/WebDevelopment/JS/Basics/75.weatherApp/index.js
@@ -77,6 +77,8 @@ function getWeatherEmoji(weatherId){
     switch(true){
         case(weatherId >= 200 && weatherId < 300):
             return "⛈️"
+        case(weatherId >= 300 && weatherId < 400):
+            return "⛈️"
     }
 }
 
