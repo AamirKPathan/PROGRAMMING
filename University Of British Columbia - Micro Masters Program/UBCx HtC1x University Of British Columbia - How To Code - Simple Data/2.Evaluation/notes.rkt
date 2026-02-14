fe2fffc8b89e60664b9e4c002c208a4f0858966b
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-02-14 01:36:47 +0000

    Feb 13, 2026, 8:36 PM

--- University Of British Columbia - Micro Masters Program/UBCx HtC1x University Of British Columbia - How To Code - Simple Data/2.Evaluation/notes.rkt
+++ University Of British Columbia - Micro Masters Program/UBCx HtC1x University Of British Columbia - How To Code - Simple Data/2.Evaluation/notes.rkt
@@ -1,5 +1,5 @@
 ;Sample Equation
-(+2 (* 3 4) (- (+ 1 2) 3))
+(+  2 (* 3 4) (- (+ 1 2) 3))
 
 ; the opening sign of a bracket pair is the primitive call
 ; it is the operator
@@ -10,4 +10,4 @@
 ; 2. Apply Primitive Calls To The Values
 
 (+ 2 (* 3 4) (- (+ 1 2) 3))
-(+2)
+(+ 2 12)
